d88241f1356349f75be339b32a0675c8c337d46d
DepreciatedAss <patelamaan0912@gmail.com>
2026-08-12 17:44:34 +0530

    Add embassy access package

---
+++ access/__init__.py
@@ -0,0 +1,5 @@
+"""Embassy access and assignment domain."""
+
+from access.service import AccessService, AssignmentResult, RevokeResult
+
+__all__ = ["AccessService", "AssignmentResult", "RevokeResult"]
