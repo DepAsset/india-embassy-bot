7ab5f588a993358452782868c8ec1f9e37846b0a
DepreciatedAss <patelamaan0912@gmail.com>
2026-08-14 15:46:25 +0530

    chore: purge legacy system

--- access/__init__.py
+++
@@ -1,5 +0,0 @@
-"""Embassy access and assignment domain."""
-
-from access.service import AccessService, AssignmentResult, RevokeResult
-
-__all__ = ["AccessService", "AssignmentResult", "RevokeResult"]
